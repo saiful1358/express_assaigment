bf45b982c596053e16ea9fdeda5cf93e6319739a
saiful1358 <saifuddinmahmud74@gmail.com>
2024-11-05 22:29:02 +0600

    Create blogController.js

---
+++ app/controllers/blogController.js
@@ -0,0 +1,28 @@
+
+// Create
+
+export const createblog =  async (req, res) => {
+
+   
+
+
+
+    return res.json({message:'Blog read successfully'});
+}
+
+// Read
+export const readblog =  async (req, res) => {
+    return res.json({message:'Blog read successfully'});
+}
+
+
+// Update
+export const updateblog =  async (req, res) => {
+    return res.json({message:'Blog update successfully'});
+}
+
+
+// Delete
+export const deleteblog =  async (req, res) => {
+    return res.json({message:'Blog delete successfully'});
+}
